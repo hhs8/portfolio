2d7914dd527b25f2a3089ff35530d0f942130bf7
hhs <hhs0528max@gmail.com>
2022-03-29 07:07:14 +0800

    chore: add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
+        transparent: "transparent",
         "apple-close": "#fd6456",
         "apple-min": "#fcbe36",
         "apple-zoom": "#53cb43",
         "apple-tab": "#404040",
         "apple-bg": "#2e2e30"
-      }
+      },
     },
   },
   plugins: [],
